1f8b210705a3b82c81a2ddc53220e581bb5dd874
小野隆規 <tkkydrm3tn@yahoo.co.jp>
2024-02-04 14:21:56 +0900

    refact: utilsの中のディレクトリ名変更

--- src/utils/register-emoji/get-token-and-cookie.js
+++ src/utils/register-emoji/get-token-and-cookie.js
@@ -1,12 +1,3 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
-
-// 環境変数の読み込み
-require("dotenv").config();
-const workspace = process.env.SLACK_WORKSPACE;
-const email = process.env.EMAIL;
-const password = process.env.PASSWORD;
-
 /**
  * Seleniumを使ってサインインしてCookieとAPIトークンを取得する関数.
  * @returns {Promise<object>} - CookieとAPIトークン
@@ -26,51 +17,6 @@ async function getCookieAndToken() {
     await driver.quit();
   }
   return { cookie, token };
-}
-
-/**
- * Seleniumのドライバーをビルドする関数
- * @returns {WebDriver} - Seleniumのドライバー
- */
-async function buildSeleniumDriver() {
-  // ヘッドレスモードのオプションを設定
-  const options = new chrome.Options();
-  options.headless();
-
-  // ヘッドレスモードでChromeを起動
-  return await new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(options)
-    .build();
-}
-
-/**
- * Slackにサインインする関数
- * @param {WebDriver} driver - Seleniumのドライバー
- * @returns {Promise<void>} - Promise
- */
-async function signInToSlack(driver) {
-  console.log("Signing in to Slack...");
-
-  // Slackにアクセス
-  await driver.get(
-    `https://${workspace}.slack.com/sign_in_with_password?redir=%2Fcustomize%2Femoji#/`
-  );
-
-  // ログイン処理
-  await driver.findElement(By.id("email")).sendKeys(email);
-  await driver.findElement(By.id("password")).sendKeys(password, Key.RETURN);
-
-  // 'boot_data'を含むscriptタグが読みこまれるのを待機 (api-tokenを取得するのに必要)
-  await driver.wait(
-    until.elementLocated(By.xpath("//script[contains(text(), 'boot_data')]")),
-    10000
-  );
-
-  // カスタム絵文字セクションが読み込まれるの待機
-  await driver.wait(until.elementLocated(By.id("list_emoji_section")), 10000);
-
-  console.log("completed sign in to Slack");
 }
 
 /**
